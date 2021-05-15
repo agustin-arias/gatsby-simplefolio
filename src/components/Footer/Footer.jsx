2525e850c88960f0431600680948ca5f4044b197
Agustin Arias <agustinntarias@github.com>
2021-05-15 11:56:08 -0300

    Added more projects and made some changes to the styles

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,14 +2,11 @@ import React, { useContext } from 'react';
 import { Container } from 'react-bootstrap';
 import { Link } from 'react-scroll';
 import PortfolioContext from '../../context/context';
-import GithubButtons from '../GithubButtons/GithubButtons';
-
-import { githubButtons } from '../../mock/data';
 
 const Footer = () => {
   const { footer } = useContext(PortfolioContext);
   const { networks } = footer;
-  const { isEnabled } = githubButtons;
+  // const { isEnabled } = githubButtons;
 
   return (
     <footer className="footer navbar-static-bottom">
@@ -36,7 +33,7 @@ const Footer = () => {
               );
             })}
         </div>
-        <hr />
+        {/* <hr />
         <p className="footer__text">
           © {new Date().getFullYear()} - Template developed by{' '}
           <a href="https://github.com/cobidev" target="_blank" rel="noopener noreferrer">
@@ -44,7 +41,7 @@ const Footer = () => {
           </a>
         </p>
 
-        {isEnabled && <GithubButtons />}
+        {isEnabled && <GithubButtons />} */}
       </Container>
     </footer>
   );
--- src/mock/data.js
+++ src/mock/data.js
@@ -29,11 +29,52 @@ export const projectsData = [
   {
     id: nanoid(),
     img: 'project.jpg',
-    title: '',
-    info: '',
-    info2: '',
-    url: '',
+    title: 'DevConnector',
+    info: 'A Social Media For Developers',
+    info2:
+      'This is a MERN stack application. It is a small social network app that includes authentication, profiles and forum posts.',
+    url: 'https://dev-connect-anta.herokuapp.com/',
     repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+  },
+  {
+    id: nanoid(),
+    img: 'project.jpg',
+    title: 'Assembler Interpreter',
+    info: 'Program designed to interpret basic Assembler code.',
+    info2:
+      "I have coded a program capable of interpreting assembler code and showing a GUI detailing how the program is working. This script uses 0 libraries with the exception of python's sys for console i/o & re for parsing input messages.",
+    url: '',
+    repo: 'https://github.com/agustin-arias/assembler_interpreter', // if no repo, the button will not show up
+  },
+  {
+    id: nanoid(),
+    img: 'project.jpg',
+    title: 'Simple Markdown Previewer',
+    info: '',
+    info2:
+      'Simple React Markdown Previewer made with the help of React,together with TypeScript, Bootstrap, font-awesome and someother npm packages.  This web application was deployed toNetlify.',
+    url: 'https://simple-react-md-previewer-anta.netlify.app/',
+    repo: 'https://github.com/agustin-arias/simple-react-md-preview', // if no repo, the button will not show up
+  },
+  {
+    id: nanoid(),
+    img: 'project.jpg',
+    title: 'Random Quote Generator',
+    info: '',
+    info2:
+      'Random quote generator made with the help of React, togetherwith Bootstrap, font-awesome and deployed to Netlify.',
+    url: 'https://random-quote-generator-anta.netlify.app/',
+    repo: 'https://github.com/agustin-arias/random-quote-generator', // if no repo, the button will not show up
+  },
+  {
+    id: nanoid(),
+    img: 'project.jpg',
+    title: 'Nurikabe Solver',
+    info: '',
+    info2:
+      'Each puzzle consists of a grid containing clues in various places.The objective is to create islands by partitioning between clueswith walls so that a number of rules are satisfied. This uses abacktracking algorithmto solve the problem.',
+    url: '',
+    repo: 'https://github.com/agustin-arias/nurikabe-solver', // if no repo, the button will not show up
   },
   {
     id: nanoid(),
